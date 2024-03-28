c94356231c191a467afddce403f8c3ceca3881d9
soufiane-amt <soufianeamt11@gmail.com>
2024-03-28 17:38:16 +0000

    Add config file

---
+++ Makefile
@@ -0,0 +1,11 @@
+TMP_RES_DIR=./tmp_results/
+TOOL_RES_DIR=./tools_extracted_result/
+
+
+
+all: clean
+
+clean:
+    rm -rf ${TMP_RES_DIR}
+    rm -rf ${TOOL_RES_DIR}
+    rm -f *_total_domains.txt
--- domExtractor.py
+++ domExtractor.py
@@ -2,16 +2,16 @@ import sys
 import subprocess
 import multiprocessing
 import os
+import yaml
 
-main_domain = sys.argv[1]
-tmp_dir = "./tmp_results/"
-tools_result = "./tools_extracted_result/"
 
-tools = {
-    "amass": ["amass", "enum", "-passive", "-d", main_domain],
-    "subfinder": ["subfinder", "-d", main_domain],
-    # Add more tools and their respective commands here
-}
+def parse_config(config_file):
+    with open(config_file, "r") as file:
+        config = yaml.safe_load(file)
+        tools = {}
+        for tool in config['tools']:
+            tools[tool['name']] = [tool['command'].format(domain=sys.argv[1]), tool['timeout']]
+        return tools
 
 def extract_subdomains(input_domain, tool_output):
     subdomains = []
@@ -23,22 +23,19 @@ def extract_subdomains(input_domain, tool_output):
                     subdomains.append(part)
     return subdomains
 
-
 def run_tool(tool_name, tool_command, output_file_path, timeout):
-    # Create a temp file that contains the output with tool name's prefix 
     with open(os.devnull, 'w') as devnull:
         try:
             print(f'{tool_name} is running ... ')
-            with open(f"{tmp_dir}{output_file_path}", "w") as output_file:
+            with open(output_file_path, "w") as output_file:
                 subprocess.run(tool_command, stdout=output_file, stderr=devnull, timeout=timeout)
         except subprocess.TimeoutExpired:
             print(f"Command {tool_command} timed out. Continuing with the script...")
 
-    # Read the temp file , parse and extract domains
-    with open(f"{tmp_dir}{output_file_path}", "r") as output_file:
+    with open(output_file_path, "r") as output_file:
         tool_output = output_file.read()
 
-    result = extract_subdomains(main_domain, tool_output)
+    result = extract_subdomains(sys.argv[1], tool_output)
 
     result_file_path = f"{tools_result}{tool_name}_subdomains.txt"
     print(f'Creating file of {tool_name} in {result_file_path}')
@@ -46,29 +43,36 @@ def run_tool(tool_name, tool_command, output_file_path, timeout):
         for subdomain in result:
             result_file.write(subdomain + '\n')
 
-    print(f"Subdomains originating from {main_domain} using {tool_name} have been saved to {result_file_path}")
-
+    print(f"Subdomains originating from {sys.argv[1]} using {tool_name} have been saved to {result_file_path}")
 
 def collect_domains_in_single_result_file():
     unique_domains = set()
-    with open(f"{main_domain}_total_domains.txt", "a") as output_file:
+    with open(f"{sys.argv[1]}_total_domains.txt", "a") as output_file:
         for tool_name, tool_command in tools.items():
             with open(f"{tools_result}{tool_name}_subdomains.txt", "r") as tool_output:
                 for domain in tool_output:
                     if domain not in unique_domains:
-                        output_file.write(domain + '\n')
+                        output_file.write(domain)
                         unique_domains.add(domain)
 
-
-
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <domain>")
+        sys.exit(1)
+
+    tools_result = "./tools_extracted_result/"
+    config_file = "config.yaml"
+    main_domain = sys.argv[1]
+    tools = parse_config(config_file)
+
     processes = []
-    for tool_name, tool_command in tools.items():
+    for tool_name, (tool_command, timeout) in tools.items():
         output_file_tmp = f"{tool_name}_{main_domain}_tmp.txt"
-        process = multiprocessing.Process(target=run_tool, args=(tool_name, tool_command, output_file_tmp, 60*3))
+        process = multiprocessing.Process(target=run_tool, args=(tool_name, tool_command, output_file_tmp, timeout))
         processes.append(process)
         process.start()
 
     for process in processes:
         process.join()
+
     collect_domains_in_single_result_file()
